c3d6016f3218d3ea5019972a2d1f1a4b4e80b06b
jong2yu <97148584+jong2yu@users.noreply.github.com>
2022-02-21 18:42:49 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const clockTarget = document.getElementById("clock");
+function clock() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  
+  clockTarget .innerText = `${hours < 10 ? `0${hours}` : hours} : ${minutes < 10 ? `0${minutes }`  : minutes } : ${seconds < 10 ? `0${seconds }`  : seconds }` ;
+}
+
+function init() {
+  clock();
+  setInterval(clock, 1000);
+}
+
+init();
